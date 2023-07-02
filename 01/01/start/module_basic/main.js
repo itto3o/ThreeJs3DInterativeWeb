258bf21096b4918cc480e5e035bcf199d85a57d4
itto3o <208ehd@naver.com>
2023-07-02 10:35:37 +0900

    .git/ remove, and reinit

---
+++ 01/01/start/module_basic/main.js
@@ -0,0 +1,8 @@
+// import { hello1, hello2 } from "./hello.js";
+// import { hello2 } from "./hello.js";
+import * as hell from './hello.js';
+// import hello1 from './hello.js';
+
+// hello1();
+hell.hello1();
+hell.hello2();
---
+++ 01/01/start/test/webpack-js-html/src/main.js
@@ -0,0 +1,3 @@
+import { message } from "./message.js";
+
+message('2분코딩!');
