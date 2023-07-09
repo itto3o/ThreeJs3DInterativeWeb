34fa2e2c833482eecc21ed37c9594108ae1bc588
itto3o <208ehd@naver.com>
2023-07-09 09:37:15 +0900

    기본 장면 만들기 - Mesh

--- 02/02/basic/src/main.js
+++ 02/02/basic/src/main.js
@@ -9,7 +9,10 @@ import * as THREE from 'three';
 
 const canvas = document.querySelector('#three-canvas');
 // const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-const renderer = new THREE.WebGLRenderer({ canvas }); //이것도 가능
+const renderer = new THREE.WebGLRenderer({ 
+    canvas,
+    antialias: true
+ }); //이것도 가능
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Scene
@@ -22,5 +25,20 @@ const camera = new THREE.PerspectiveCamera(
     0.1, // near
     1000 // far
 );
+camera.position.x = 1;
+camera.position.y = 2;
 camera.position.z = 5;
 scene.add(camera);
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    // color: 0xff0000
+    // color: '#ff0000'
+    color: 'red'
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// 그리기
+renderer.render(scene, camera);
