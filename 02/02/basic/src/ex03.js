e4e7f28b8a7f1f06ead2d7fe04e46d919f50ec7e
itto3o <208ehd@naver.com>
2023-07-15 10:08:56 +0900

    배경의 색, 투명도 설정하기

---
+++ 02/02/basic/src/ex03.js
@@ -0,0 +1,77 @@
+import * as THREE from 'three';
+
+// ----- 주제: 브라우저 창 사이즈 변경에 대응하기
+
+export default function example() {
+    // Renderer
+    const canvas = document.querySelector('#three-canvas');
+    const renderer = new THREE.WebGLRenderer({ 
+        canvas,
+        antialias: true,
+        alpha: true
+     }); //이것도 가능
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // console.log(window.devicePixelRatio);
+    renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+    // renderer.setClearAlpha(0.5);
+    // renderer.setClearColor(0x00ff00);
+    // renderer.setClearColor('#00ff00');
+    // renderer.setClearAlpha(0.5);
+    
+    // Scene
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('blue');
+    
+    // Camera
+    // const camera = new THREE.PerspectiveCamera(
+    //     75, // 시야각(field of view)
+    //     window.innerWidth / window.innerHeight, // 종횡비(aspect)
+    //     0.1, // near
+    //     1000 // far
+    // );
+    // camera.position.x = 1;
+    // camera.position.y = 2;
+    // camera.position.z = 5;
+    
+    // Orthographic Camera
+    const camera = new THREE.OrthographicCamera(
+        -(window.innerWidth / window.innerHeight), // left
+        window.innerWidth / window.innerHeight, // right
+        1,
+        -1,
+        0.1,
+        1000
+    );
+    camera.position.x = 1;
+    camera.position.y = 2;
+    camera.position.z = 5;
+    camera.lookAt(0, 0, 0);
+    camera.zoom = 0.5;
+    camera.updateProjectionMatrix();
+    scene.add(camera);
+    
+    // Mesh
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({
+        // color: 0xff0000
+        // color: '#ff0000'
+        color: 'red'
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    
+    // 그리기
+    renderer.render(scene, camera);
+
+    function setSize() {
+        // 카메라
+        camera.aspect = window.innerWidth / window.innerHeight;
+        // updateProjectionMatrix 카메라 투영에 관련된 값에 변화가 있을 경우 실행해야 함
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.render(scene, camera);
+    }
+    // 이벤트
+    window.addEventListener('resize', setSize);
+}
+
