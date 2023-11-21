65d570fb4f15b5ebd3ff89afad9008ee1e6ba43f
itto3o <208ehd@naver.com>
2023-11-21 18:50:46 +0900

    다운로드 받은 파일 활용하기

--- 10/10/custom_model/src/ex01.js
+++ 10/10/custom_model/src/ex01.js
@@ -43,7 +43,8 @@ export default function example() {
 	// gltf loader
 	const gltfLoader = new GLTFLoader();
 	gltfLoader.load(
-		'/models/ilbuni.glb',
+		// '/models/ilbuni.glb',
+		'/models/roncho.glb',
 		gltf => {
 			// console.log(gltf.scene.children[0]);
 			const ilbuniMesh = gltf.scene.children[0];
