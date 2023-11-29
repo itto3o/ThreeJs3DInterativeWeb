1fb27f2c7ec9ef303b8626ce36111d41c8d4f6c7
itto3o <208ehd@naver.com>
2023-11-29 20:05:31 +0900

    물리엔진 월드 생성성

---
+++ 11/11/physics/src/ex01.js
@@ -0,0 +1,90 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as CANNON from 'cannon-es';
+
+// ----- 주제: cannon.js 기본 세팅
+
+// cannon.js 문서
+// http://schteppe.github.io/cannon.js/docs/
+// 주의! https 아니고 http
+
+export default function example() {
+	// Renderer
+	const canvas = document.querySelector('#three-canvas');
+	const renderer = new THREE.WebGLRenderer({
+		canvas,
+		antialias: true
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+	// Scene
+	const scene = new THREE.Scene();
+
+	// Camera
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	camera.position.y = 1.5;
+	camera.position.z = 4;
+	scene.add(camera);
+
+	// Light
+	const ambientLight = new THREE.AmbientLight('white', 0.5);
+	scene.add(ambientLight);
+
+	const directionalLight = new THREE.DirectionalLight('white', 1);
+	directionalLight.position.x = 1;
+	directionalLight.position.z = 2;
+	scene.add(directionalLight);
+
+	// Controls
+	const controls = new OrbitControls(camera, renderer.domElement);
+
+	// Cannon(물리 엔진)
+	const cannonWorld = new CANNON.World();
+	cannonWorld.gravity.set(0, -10, 0);
+
+	// Mesh
+	const floorMesh = new THREE.Mesh(
+		new THREE.PlaneGeometry(10, 10),
+		new THREE.MeshStandardMaterial({
+			color: 'slategray'
+		})
+	);
+	floorMesh.rotation.x = -Math.PI / 2;
+	scene.add(floorMesh);
+
+	const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+	const boxMaterial = new THREE.MeshStandardMaterial({
+		color: 'seagreen'
+	});
+	const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+	boxMesh.position.y = 0.5;
+	scene.add(boxMesh);
+
+	// 그리기
+	const clock = new THREE.Clock();
+
+	function draw() {
+		const delta = clock.getDelta();
+
+		renderer.render(scene, camera);
+		renderer.setAnimationLoop(draw);
+	}
+
+	function setSize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.render(scene, camera);
+	}
+
+	// 이벤트
+	window.addEventListener('resize', setSize);
+
+	draw();
+}
